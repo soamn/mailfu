99894b3ba4b36185cdc80d4f64087e150412bb5c
soamn <negiaman604@gmail.com>
2024-10-17 14:15:38 +0530

    dev init

--- lib/db.ts
+++ lib/db.ts
@@ -8,9 +8,7 @@ export const prisma =
   global.prisma ||
   new PrismaClient({
     log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "info", "warn", "error"]
-        : [],
+      process.env.NODE_ENV === "development" ? ["query", "warn", "error"] : [],
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
