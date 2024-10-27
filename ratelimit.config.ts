9307c83e56e3219d76fb11ea32d5ebba57832ba8
soamn <negiaman604@gmail.com>
2024-10-27 19:16:14 +0530

    minor fix

--- ratelimit.config.ts
+++ ratelimit.config.ts
@@ -1,6 +1,6 @@
 import { Ratelimit } from "@unkey/ratelimit";
 export const quickMailLimiter = new Ratelimit({
-  rootKey: process.env.UNKEY_ROOT_KEY,
+  rootKey: process.env.UNKEY_ROOT_KEY as string,
   namespace: "mailfu",
   limit: 3,
   duration: 10000 * 60,
@@ -8,7 +8,7 @@ export const quickMailLimiter = new Ratelimit({
 });
 
 export const mailLimiter = new Ratelimit({
-  rootKey: process.env.UNKEY_ROOT_KEY,
+  rootKey: process.env.UNKEY_ROOT_KEY as string,
   namespace: "mailfu",
   limit: 20,
   duration: 10000 * 60 * 10,
